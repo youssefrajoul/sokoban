366d9bec1519bec4445bc92844b77374d77b4265
59939 <59939@etu.he2b.be>
2022-04-07 13:53:16 +0200

    Étape 2.2 : Comprendre l'encodage

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,9 @@
-console.log("hello world");
-document.getElementById('body');
-
-$(".bar").first();
-$("#id").addClass("test");
+/**
+ * This function display map from levels Object in browser console
+ * @param {number} level
+ */
+function buildLevel(level) {
+     for (let i = 0; i < levels[level].map.length; i++) {
+         console.log(levels[level].map[i]);
+     }
+}
