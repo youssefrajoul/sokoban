ee9995fff8519fd139e82a1a99ec35e60955b9cc
59939 <59939@etu.he2b.be>
2022-04-19 15:05:38 +0200

    Étape 2.3 : Préparation

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,8 @@
  * This function display map from levels Object in browser console
  * @param {number} level
  */
-function buildLevel(level) {
-     for (let i = 0; i < levels[level].map.length; i++) {
-         console.log(levels[level].map[i]);
-     }
+ function buildLevel(level) {
+    for (let i = 0; i < levels[level].map.length; i++) {
+        console.log(levels[level].map[i]);
+    }
 }
