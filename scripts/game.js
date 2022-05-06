93221eef779226fc3d231542f9e9b909fff303a0
59939 <59939@etu.he2b.be>
2022-05-06 12:57:12 +0200

    Étape 4.7 : Dialogue d’aide

--- scripts/game.js
+++ scripts/game.js
@@ -199,3 +199,21 @@ $("#restart").on("click", function () {
 window.addEventListener("keydown", (e) => {
     move(e);
 });
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector("#trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
